e8e4ec357dff4b584cf20a863f73ec5ee85eadbb
aragrev <ruben.vergara@live.cl>
2016-12-11 19:11:49 -0300

    Platform v3
    modificado 11-12-16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { LoginPage } from '../pages/login/login';
+import { RegistroUsuarioPage } from '../pages/registro-usuario/registro-usuario'
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    LoginPage,
+    RegistroUsuarioPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +16,8 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    LoginPage,
+    RegistroUsuarioPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
