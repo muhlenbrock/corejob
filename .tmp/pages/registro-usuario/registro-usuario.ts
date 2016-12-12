b3e043dbfea7d27d629a495b6e7441308df2613c
aragrev <ruben.vergara@live.cl>
2016-12-11 22:15:09 -0300

    Malo

---
+++ .tmp/pages/registro-usuario/registro-usuario.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the RegistroUsuario page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-registro-usuario',
+  templateUrl: 'registro-usuario.html'
+})
+export class RegistroUsuarioPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('Hello RegistroUsuarioPage Page');
+  }
+
+
+}
